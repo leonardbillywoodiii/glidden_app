f07c7fbfc1be555baee3b3e9b6a9357acfd0c298
bhwood <wood.outlook.com>
2020-05-01 16:22:27 -0500

    Format update VSCODE to AutoPEP8

--- cms/models.py
+++ cms/models.py
@@ -13,7 +13,9 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=email,
+                          first_name=first_name,
+                          last_name=last_name)
 
         user.set_password(password)
         user.save(using=self._db)
