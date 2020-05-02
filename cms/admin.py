dd03b1cd05eedc490c4d9cd7955823409fb7c9d8
bhwood <wood.outlook.com>
2020-05-02 15:22:58 -0500

    Added test for Admin page test_users_listed

--- cms/admin.py
+++ cms/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from cms import models
 
-admin.site.register(models.UserProfile)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'last_name', 'first_name']
+
+
+admin.site.register(models.UserProfile, UserAdmin)
---
+++ cms/tests/test_admin_page.py
@@ -0,0 +1,32 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setup(self):
+        self.client = Client()
+        self.admmin_user = get_user_model().objects.create_superuser(
+            email='test.admin@test.com',
+            first_name='Elias',
+            last_name='Jackson',
+            password='testpassword'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test.user@test.com',
+            first_name='John',
+            last_name='Smith',
+            password='anotherpassword'
+        )
+
+    def test_users_listed(self):
+        self.setup()
+        url = reverse('admin:cms_userprofile_changelist')
+        responce = self.client.get(url)
+
+        self.assertContains(responce, self.user.first_name)
+        self.assertContains(responce, self.user.last_name)
+        self.assertContains(responce, self.user.email)
