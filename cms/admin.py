41bb8d74f4776517a182928c086c841c9060105d
bhwood <wood.outlook.com>
2020-05-02 18:12:34 -0500

    Added fieldsets for Admin Edit page

--- cms/admin.py
+++ cms/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from cms import models
+from django.utils.translation import gettext
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'last_name', 'first_name']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email', 'password',
+            ),
+        }),
+        (gettext('Personal Info'), {
+            'fields': (
+                'first_name', 'last_name',
+            ),
+        }),
+        (gettext('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+            ),
+        }),
+        (gettext('Important Dates'), {
+            'fields': (
+                'last_login',
+            ),
+        }),
+    )
 
 
 admin.site.register(models.UserProfile, UserAdmin)
