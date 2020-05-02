f8c729842c76af5062149054bf80695957e15d99
bhwood <wood.outlook.com>
2020-05-02 15:35:56 -0500

    Updated test_users_listed

--- cms/tests/test_admin_page.py
+++ cms/tests/test_admin_page.py
@@ -7,7 +7,7 @@ class AdminSiteTests(TestCase):
 
     def setup(self):
         self.client = Client()
-        self.admmin_user = get_user_model().objects.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email='test.admin@test.com',
             first_name='Elias',
             last_name='Jackson',
@@ -26,6 +26,10 @@ class AdminSiteTests(TestCase):
         self.setup()
         url = reverse('admin:cms_userprofile_changelist')
         responce = self.client.get(url)
+
+        self.assertContains(responce, self.admin_user.first_name)
+        self.assertContains(responce, self.admin_user.last_name)
+        self.assertContains(responce, self.admin_user.email)
 
         self.assertContains(responce, self.user.first_name)
         self.assertContains(responce, self.user.last_name)
