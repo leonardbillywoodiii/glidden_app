6807c0ffcc30fa95b58f09fc57a6ab21e9b46ce7
bhwood <wood.outlook.com>
2020-05-03 21:00:45 -0500

    Added __str__ to PhoneNumber

--- cms/models.py
+++ cms/models.py
@@ -70,5 +70,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
 class PhoneNumber(models.Model):
     number = models.CharField(max_length=20)
-    number_type = models.CharField(max_length=20)
+    number_type = models.CharField(max_length=20, default='Cell')
     UserProfile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    REQUIRED_FIELDS = ['number', 'number_type']
+
+    def __str__(self):
+        """Return String representation as Cell: 936.555.1234"""
+        return "{}: {}.{}.{}".format(
+            self.number_type,
+            self.number[:3],
+            self.number[3:6],
+            self.number[6:]
+        )
