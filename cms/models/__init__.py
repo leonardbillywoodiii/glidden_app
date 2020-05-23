26caace30e3cff8367c15236ab0e446cd21faa75
Leonard Wood <wood.billy@outlook.com>
2020-05-23 16:04:32 -0500

    Added Event Model

--- cms/models/__init__.py
+++ cms/models/__init__.py
@@ -1,12 +1,13 @@
+from .Event import Event
 from .GeneralAddress import GeneralAddress
 from .MemberAddress import MemberAddress
 from .Ministry import Ministry
 from .MinistryLeader import MinistryLeader
+from .MinistryTime import MinistryTime
 from .PhoneNumber import PhoneNumber
 from .UserProfile import UserProfile, UserProfileManager
-from .MinistryTime import MinistryTime
 
 __all__ = ['GeneralAddress', 'MemberAddress', 'Ministry',
            'PhoneNumber', 'UserProfile', 'UserProfileManager',
-           'MinistryLeader', 'MinistryTime'
+           'MinistryLeader', 'MinistryTime', 'Event'
            ]
