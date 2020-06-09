4cbbab1dddc398484dd88babe2fb2a8585e25af9
Leonard Wood <wood.billy@outlook.com>
2020-06-09 18:38:17 -0500

    Reformat of document added flake8 line comment

--- cms/tests/test_model_media.py
+++ cms/tests/test_model_media.py
@@ -19,7 +19,8 @@ class MediaModelTest(TestCase):
     def test_media_created(self):
         faker = Faker('en_US')
         md5 = hashlib.md5()
-        with open(os.getcwd() + '/cms/tests/test_media/2954199.jpg', 'rb') as f:
+        with open(os.getcwd() +
+                  '/cms/tests/test_media/2954199.jpg', 'rb') as f:
             while True:
                 data = f.read(self.BUFFER_SIZE)
                 if not data:
@@ -30,10 +31,8 @@ class MediaModelTest(TestCase):
             title=faker.sentence(nb_words=4),
             description=faker.paragraph(nb_sentences=3),
             media_file=SimpleUploadedFile(name=md5.hexdigest() + '.jpg',
-                                          content=open(
-                                              os.getcwd() + '/cms/tests/test_media/2954199.jpg', 'rb')
-                                          .read(),
-                                          content_type='image/jpeg'),
+                                          content=open(os.getcwd() + '/cms/tests/test_media/2954199.jpg', 'rb')  # noqa: E501
+                                          .read(), content_type='image/jpeg'),
             UserProfile=self.admin_user
         )
         test_media.save()
